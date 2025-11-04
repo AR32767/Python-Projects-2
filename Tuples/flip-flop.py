ea75b31c0aabfef99695e914a376c3de0ee4395c
AR32767 <arjun05.nair@gmail.com>
2025-11-04 16:53:28 -0500

    commit

--- Tuples/flip-flop.py
+++ Tuples/flip-flop.py
@@ -1,8 +1,6 @@
 tuple1 = (1,2,3,3,2,1)
-tuple2 = slice(0,len(tuple1)/2+1)
-tuple3 = slice(len(tuple1)/2+2,len(tuple1)+1)
-mtuple3=slice(0,-len(tuple3)-1,-1)
-if tuple2 == mtuple3:
+
+if tuple1 == tuple1[::-1]:
     print("Palindrome")
 else:
-    print("No palindrome")
+    print("Not a palindrome")
